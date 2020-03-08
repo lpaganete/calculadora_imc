1d64f18be92c47430e45af2cd6b8c19dc529f905
Luis Paulo Aganete <luispaulo.aganete@hotmail.com>
2020-03-08 18:50:39 -0300

    Criando funções para os textField (altura e peso)

--- lib/widget/textField_numbers.dart
+++ lib/widget/textField_numbers.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
 
-class TextFieldNumber extends StatelessWidget {
-  
+class TextFieldNumber extends StatefulWidget {
+
   String text;
-  TextEditingController controller;
+  TextEditingController controladora;
 
-  TextFieldNumber(this.text, {@required this.controller});
+  TextFieldNumber(this.text, this.controladora);
 
+  @override
+  _TextFieldNumberState createState() => _TextFieldNumberState();
+}
+
+class _TextFieldNumberState extends State<TextFieldNumber> {
+  
+  get controladora => null;
   @override
   Widget build(BuildContext context) {
     return TextField(
-      
-      keyboardType: TextInputType.number,
-      decoration: InputDecoration(
-        labelText: text,
-        labelStyle: TextStyle(color: Colors.yellow[900]),
-      ),
-      textAlign: TextAlign.center,
-      style: TextStyle(color: Colors.yellow[900], fontSize: 25.0),
-    );
+          controller: controladora,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            labelText: "Peso (kg)",
+            labelStyle: TextStyle(color: Colors.yellow[900]),
+          ),
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.yellow[900], fontSize: 25.0),
+        );
   }
 }
